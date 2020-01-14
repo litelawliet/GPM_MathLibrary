0732f952b0dd3cb33386fd3349fe46050fd37266
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2020-01-14 14:54:20 -0500

    Rotation, Orthographic matrix added. Documentation added

--- GPM/MathTestProject/src/main.cpp
+++ GPM/MathTestProject/src/main.cpp
@@ -6,9 +6,9 @@ int main(const int p_argc, char** p_argv)
 	try
 	{
 		// Try stuff here
-		Matrix4F proj = GPM::Matrix4F::Perspective(GPM::Tools::Utils::ToRadians(45.0f), 800/static_cast<float>(600), 0.1f, 10.0f);
-		proj.m_data[5] = proj[5] * -1;
-		std::cout << proj;
+		Matrix4F rot = GPM::Matrix4F::Orthographic(0, 800, 600, 0);
+
+		std::cout << rot;
 	}
 	catch (const std::exception & e)
 	{
